5ca8ba41d27f2962197cac198501571c192a3d8d
Juan Carlos Girón Pozuelos <jcgiron@ufm.edu>
2021-08-04 14:22:03 -0600

    esto es una prueba

---
+++ intro-r/script_intro.R
@@ -0,0 +1,18 @@
+## instalar librerias
+
+install.packages("dplyr")
+install.packages("RMySQL")
+install.packages("lubridate")
+install.packages("openxlsx")
+install.packages("tidyverse")
+install.packages("stringr")
+install.packages("readr")
+
+
+###cargar librerias
+
+library(dplyr)
+require(dplyr)
+
+###folder location
+getwd()
